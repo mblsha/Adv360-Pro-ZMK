2c81d4efe2646fd1958808b46d8d7bf900958f4d
Michael Pishchagin <mblsha@google.com>
2023-07-30 12:50:53 +1000

    add some modifiers

--- bin/build.sh
+++ bin/build.sh
@@ -9,10 +9,12 @@ TIMESTAMP="${TIMESTAMP:-$(date -u +"%Y%m%d%H%M%S")}"
 west build -s zmk/app -d build/left -b adv360_left -- -DZMK_CONFIG="${PWD}/config"
 # Adv360 Left Kconfig file
 cat build/left/zephyr/.config | grep -v "^#" | grep -v "^$"
+
 # West Build (right)
 west build -s zmk/app -d build/right -b adv360_right -- -DZMK_CONFIG="${PWD}/config"
 # Adv360 Right Kconfig file
 cat build/right/zephyr/.config | grep -v "^#" | grep -v "^$"
+
 # Rename zmk.uf2
 cp build/left/zephyr/zmk.uf2  ./firmware/${TIMESTAMP}-left.uf2
 cp build/left/zephyr/zmk.uf2  ./firmware/left.uf2
