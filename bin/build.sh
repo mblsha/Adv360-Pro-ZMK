35bbe9175d2be9ec70246a0feffe34cc837801f3
Michael Pishchagin <mblsha@google.com>
2023-07-30 14:38:10 +1000

    format layers to have uniform per-key width

--- bin/build.sh
+++ bin/build.sh
@@ -8,12 +8,12 @@ TIMESTAMP="${TIMESTAMP:-$(date -u +"%Y%m%d%H%M%S")}"
 # West Build (left)
 west build -s zmk/app -d build/left -b adv360_left -- -DZMK_CONFIG="${PWD}/config"
 # Adv360 Left Kconfig file
-cat build/left/zephyr/.config | grep -v "^#" | grep -v "^$"
+cat build/left/zephyr/.config | grep -v "^#" | grep -v "^$" > ./firmware/left.kconfig
 
 # West Build (right)
 west build -s zmk/app -d build/right -b adv360_right -- -DZMK_CONFIG="${PWD}/config"
 # Adv360 Right Kconfig file
-cat build/right/zephyr/.config | grep -v "^#" | grep -v "^$"
+cat build/right/zephyr/.config | grep -v "^#" | grep -v "^$" > ./firmware/right.kconfig
 
 # Rename zmk.uf2
 cp build/left/zephyr/zmk.uf2  ./firmware/${TIMESTAMP}-left.uf2
